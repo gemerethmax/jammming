b1e40e2b919cb1859b62361e38d2de6f0269ef1c
Max Gemereth <gemereth.max@gmail.com>
2024-11-25 12:17:05 -0500

    New feature: Show More Results Button

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -3,6 +3,9 @@ import Track from '../Track/Track';
 import './SearchResults.css';
 
 function SearchResults(props) {
+
+  const [count, setCount] = React.useState(0);
+
   const tracklist = props.value.map((song) => (
     <Track
       name={song.name}
@@ -16,11 +19,29 @@ function SearchResults(props) {
     />
   ));
 
+    
+
   return (
     <div>
-      <h2 class='searchResults'>Search Results</h2>
-      <div>{tracklist}</div>
-    </div>
+    <h2 className='searchResults'>Search Results</h2>
+      {count === 0 && tracklist.slice(0, 20).map((track, index) => (
+        <div key={index}>{track}</div>
+      ))}
+      {count === 1 && tracklist.slice(0, 30).map((track, index) => (
+        <div key={index}>{track}</div>
+      ))}
+      {count === 2 && tracklist.slice(0, 40).map((track, index) => (
+        <div key={index}>{track}</div>
+      ))}
+      {count === 3 && tracklist.slice(0, 50).map((track, index) => (
+        <div key={index}>{track}</div>
+      ))}
+      {count >= 4 && tracklist.slice(0, 50).map((track, index) => (
+        <div key={index}>{track}</div>
+      ))}
+      {count >= 4 && <p style={{ color: 'Red', fontSize: 24, fontWeight: 'bold'}}>Sorry, no more results available!</p>}
+        <button onClick={() => setCount(prevCount => prevCount + 1)} id="showMoreButton">Show More Results</button>
+      </div>
   );
 }
 
